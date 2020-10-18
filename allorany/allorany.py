ede6d6279ab94546db95c2fab2ac1afafc2d65ec
ARj-cyber <arjunpdinesh123@gmail.com>
2020-10-18 17:46:06 +0530

    tasks

---
+++ allorany/allorany.py
@@ -0,0 +1,10 @@
+
+a,b,c,d,e = input() .split()
+p = int(a) == True
+q = int(b) == True
+r = int(c) == True
+s = int(d) == True
+t = int(e) == True
+z = ( p and q and r and s and t )
+if z :
+	print ("...")
